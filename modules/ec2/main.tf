4ca536aaa2d849693382fe151af62c5a4f390e7f
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-03 17:12:34 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "openproject" {
     apt-get install -y docker.io
     systemctl start docker
     systemctl enable docker
-    docker run -dit -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12Add commentMore actions
+    docker run -dit -p 80:8080 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HOST__NAME=0.0.0.0:80 -e OPENPROJECT_HTTPS=false openproject/community:12Add commentMore actions
   EOT
 
   tags = merge(var.tags, {
