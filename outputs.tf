b3f0625f227596dcac4db330c158afaae38d9d07
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-04 09:06:11 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,16 @@
 output "vpc_id" {
-
   value = module.vpc.vpc_id
-
 }
 
 output "subnet_1_id" {
-
   value = module.vpc.subnet_1_id
-
 }
 
 output "subnet_2_id" {
-
   value = module.vpc.subnet_2_id
-
 }
 
 output "ec2_security_group" {
-
   value = module.sg.sg_id
-
 }
  
